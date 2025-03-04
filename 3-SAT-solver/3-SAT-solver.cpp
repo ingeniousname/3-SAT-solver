d4a119e8ff66583a2b6da1183ce465b1f2eaa67c
ingeniousname <miloszwozny5@gmail.com>
2025-03-04 15:23:14 +0100

    init

---
+++ 3-SAT-solver/3-SAT-solver.cpp
@@ -0,0 +1,11 @@
+﻿// 3-SAT-solver.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	return 0;
+}
