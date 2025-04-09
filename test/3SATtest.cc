d2e31099056a1b358450aca2c64034955f2b164e
adam <adamowski137@gmail.com>
2025-04-09 21:10:51 +0200

    Add tests

--- test/3SATtest.cc
+++ test/3SATtest.cc
@@ -36,3 +36,64 @@ TEST(SATtest, Base3VarFalse) {
 	EXPECT_FALSE(Solver::solveAUX(input, assignment, 0));
 }
 
+
+TEST(SATtest, Base4VarTrue) {
+	Input3SAT input;
+
+	input.clauses = {
+	{ { { 0, false }, { 1, false }, { 2, false } } },
+	{ { { 0, false }, { 1, false }, { 2, true } } },
+	{ { { 0, false }, { 1, true }, { 3, false } } },
+	{ { { 0, false }, { 1, true }, { 3, true } } },
+	{ { { 0, true }, { 2, false }, { 3, false } } },
+	{ { { 0, true }, { 2, false }, { 3, true } } },
+	{ { { 0, true }, { 2, true }, { 1, false } } },
+	{ { { 0, true }, { 2, true }, { 1, true } } },
+	};
+
+	std::vector<VarState> assignment;
+	for (int i = 0; i < input.clauses.size(); i++) {
+		assignment.push_back(VarState::Unassigned);
+	}
+	EXPECT_FALSE(Solver::solveAUX(input, assignment, 0));
+}
+
+
+TEST(SATtest, Base5VarTrue) {
+	Input3SAT input;
+	input.clauses = {
+		{ { { 0, false }, { 1, false }, { 2, false } } },
+		{ { { 0, false }, { 1, false }, { 2, true } } },
+		{ { { 0, false }, { 1, true }, { 2, false } } },
+		{ { { 0, true }, { 1, false }, { 2, false } } },
+		{ { { 0, false }, { 1, true }, { 2, true } } },
+		{ { { 0, true }, { 1, false }, { 2, true } } },
+		{ { { 0, true }, { 1, true }, { 2, false } } },
+		{ { { 0, true }, { 1, true }, { 2, true } } },
+
+	};
+	std::vector<VarState> assignment;
+	for (int i = 0; i < input.clauses.size(); i++) {
+		assignment.push_back(VarState::Unassigned);
+	}
+	EXPECT_TRUE(Solver::solveAUX(input, assignment, 0));
+}
+
+
+
+TEST(SATtest, Base5VarTrue2) {
+	Input3SAT input;
+	input.clauses = {
+	{ { { 0, true }, { 3, false }, { 4, false } } },
+	{ { { 1, false }, { 2, true }, { 4, false } } },
+	{ { { 0, true }, { 1, true }, { 2, false } } },
+	};
+
+	std::vector<VarState> assignment;
+	for (int i = 0; i < input.clauses.size(); i++) {
+		assignment.push_back(VarState::Unassigned);
+	}
+	EXPECT_TRUE(Solver::solveAUX(input, assignment, 0));
+}
+
+
